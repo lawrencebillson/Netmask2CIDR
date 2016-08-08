710c6ca55b1e98f6452e57a4566d7adadecf353f
lawrencebillson <lawrencebillson@yahoo.com.au>
2016-08-08 09:35:07 +0800

    Update n2c.pl
    Bothered doing some maths, rather than just looping - unsure of which is more efficient

--- n2c.pl
+++ n2c.pl
@@ -1,11 +1,20 @@
 #!/usr/bin/perl
+#
+# Perl function to turn a dotted quad netmask into CIDR notation
+# Lawrence Billson, 2016
 
-sub n2c {
-	my $size = unpack('N', (pack 'C4', split(/\./,shift)));
-	my $cidr;
-	for ($cidr = 0; 2 ** 32 - (2 ** (32 - $cidr)) != $size; $cidr++) {};
-	return ($cidr);
-	}
 
-$data = n2c($ARGV[0]);
-print "/$data\n";
+sub netmask2cidr {
+        my $a =  unpack('N', (pack 'C4', split(/\./,shift)));
+        return 32 - (log(2**32 - $a)/log(2));
+        }
+
+
+
+$quad = $ARGV[0];
+
+print "Quad is $quad\n";
+
+$cidr = netmask2cidr($quad);
+
+print "CIRD is $cidr\n";
